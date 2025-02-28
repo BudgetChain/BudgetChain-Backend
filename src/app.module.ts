d7e65f6e5ef36fd8f4814e26bd8dc97255ce8ea4
Kalu Victor <victorbuikemdev@gmail.com>
2025-02-28 06:39:02 +0000

    Addition of Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { AiModule } from './modules/ai/ai.module';
+import { ReportingModule } from './modules/reporting/reporting.module';
+import { BlockchainModule } from './modules/blockchain/blockchain.module';
+import { BudgetModule } from './modules/budget/budget.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TreasuryModule } from './modules/treasury/treasury.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AiModule, ReportingModule, BlockchainModule, BudgetModule, AuthModule, TreasuryModule],
   controllers: [AppController],
   providers: [AppService],
 })
