c9291dea942695946ad869cf978d5ffa46cb4ee4
anonfedora <musaeleazar090@gmail.com>
2025-03-05 10:59:26 +0100

    fix: Tests workflow

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingService } from './config/logging.service';
+
+// Create a mock LoggingService
+const mockLoggingService = {
+  setContext: jest.fn(),
+  log: jest.fn(),
+  debug: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +18,14 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        // Provide the mock LoggingService
+        {
+          provide: LoggingService,
+          useValue: mockLoggingService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -17,6 +34,23 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+
+    it('should log messages when getHello is called', () => {
+      appController.getHello();
+
+      expect(mockLoggingService.setContext).toHaveBeenCalledWith('AppService');
+      expect(mockLoggingService.log).toHaveBeenCalledWith('Doing something...');
+      expect(mockLoggingService.debug).toHaveBeenCalledWith(
+        'Debugging information...',
+      );
+      expect(mockLoggingService.warn).toHaveBeenCalledWith(
+        'A warning occurred!',
+      );
+      expect(mockLoggingService.error).toHaveBeenCalledWith(
+        'An error occurred!',
+        'Stack trace or error object',
+      );
     });
   });
 });
